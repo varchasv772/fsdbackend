d426c01409d01ac47d9051e73c6864e024981a82
varchasv772 <varchu0507@gmail.com>
2025-04-17 15:09:06 +0530

    Day5

--- Day5/frontend/src/App.jsx
+++ Day5/frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import Register from './component/Register'
+import View from './component/View'
 const App = () => {
   return (
     <div>
       <h1>User Registration System</h1>
       <Register/>
-    
+       <View/>
 
     </div>
   )
